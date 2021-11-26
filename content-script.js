cf83abb2480c1da2ee14cfe541633b896fe5b3d4
startdusk <522383345@qq.com>
2021-11-26 21:29:25 +0800

    add: change youtube background color

--- content-script.js
+++ content-script.js
@@ -1,3 +1,12 @@
 window.onload = () => {
-  document.querySelector("#search-icon-legacy").prepend("你好");
+  const button = document.createElement("button");
+  button.id = "darkModeButton";
+  button.textContent = "DO IT DARK";
+  document.querySelector("#search-icon-legacy").prepend(button);
+  button.addEventListener("click", () => enableChangeColorMode());
 };
+
+function enableChangeColorMode() {
+  document.getElementsByTagName("ytd-rich-grid-renderer")[0].style.background =
+    "red";
+}
